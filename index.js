262dab4d92c6af5483b66c8ca8dae47be83454ff
bbohling <brandon@bohling.me>
2023-06-23 17:31:35 -0700

    feat: limit/version and delete

--- index.js
+++ index.js
@@ -2,42 +2,28 @@ const express = require("express");
 const Promise = require("bluebird");
 const morgan = require("morgan");
 const cors = require("cors");
-const fs = require("fs");
-const https = require("https");
-var session = require("express-session");
-var createError = require("http-errors");
-var cookieParser = require("cookie-parser");
 const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc.js");
 const { PrismaClient } = require("@prisma/client");
 const _ = require("lodash");
-const bodyParser = require('body-parser')
 const app = express();
 
 // middeware
 app.use(express.json());
-// app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("tiny"));
-// app.use(
-//   session({
-//     secret: process.env.EXPRESS_SESSION_SECRET,
-//     resave: true,
-//     saveUninitialized: false,
-//     cookie: {
-//       secure: false, // set this to true on production
-//     },
-//   })
-// );
+
 
 const prisma = new PrismaClient();
 dayjs.extend(utc);
 
 app.get("/leaderboards", async (req, res, next) => {
+  const limit = req.query.limit && Number(req.query.limit);
+  const version = req.query.version;
+
   try {
-    const leaderboards = await prisma.leaderboard.findMany();
+    const leaderboards = await prisma.leaderboard.findMany({where: { version }, take: limit});
     const results = {
       results: leaderboards?.length,
       data: leaderboards
@@ -49,7 +35,6 @@ app.get("/leaderboards", async (req, res, next) => {
 });
 
 app.post("/leaderboards", async (req, res, next) => {
-  console.log(req.body);
   try {
     const newEntry = await prisma.leaderboard.create({ data: req.body });
     res.json(newEntry);
@@ -57,6 +42,22 @@ app.post("/leaderboards", async (req, res, next) => {
     next(error);
   }
 });
+
+app.delete('/leaderboards/:id', async (req, res, next) => {
+  const token = req.params.lbToken;
+  if (process.env.LB_TOKEN && token === process.env.LB_TOKEN) {
+    try {
+      const id = Number(req.params.id);
+      const leader = await prisma.leaderboard.delete({ where: { id } });
+      res.json(leader);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
